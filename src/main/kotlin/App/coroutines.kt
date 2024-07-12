690b3aef82f7f97a99cb278ae2375a99de4f6c37
Irhan Nugroho <irhan9001@gmail.com>
2024-07-12 14:30:16 +0700

    menambahkan coroutines.kt dan juga genericClass.kt

---
+++ src/main/kotlin/App/coroutines.kt
@@ -0,0 +1,13 @@
+package App
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+fun main() = runBlocking{
+    launch {
+        delay(1000L)
+        println("Coroutines!")
+    }
+    println("Hello,")
+    delay(2000L)
+}
---
+++ src/main/kotlin/Data/dataGeneric.kt
@@ -0,0 +1,6 @@
+package Data
+
+class dataGeneric<T> {
+
+
+}
